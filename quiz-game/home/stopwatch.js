ad8905c1d8db3b007013333d70d4ae0051902a4e
Harshit <prajapati.ha@northeastern.edu>
2024-01-24 16:23:57 -0500

    Stopwatch commit

--- quiz-game/home/stopwatch.js
+++ quiz-game/home/stopwatch.js
@@ -2,11 +2,26 @@ function timer() {
     sec = 0;
     min = 0;
     let stopWatch; 
-  
+    watchStatus = "reset"; // 0-reset; 1-running; 3-pause
+
+
     function startTimer() {
+      watchStatus = "running";
+      changeStartLabel();
       stopWatch = setInterval(() => {
         updateTimer();
       }, 1000);
+    }
+
+    function changeStartLabel() {
+      if (watchStatus === "reset") {
+        startStr = "Start";
+      }else if (watchStatus === "pause") {
+        startStr = "Resume"
+      } else {
+        startStr = "Stop"
+      }
+      document.getElementById('start-btn').textContent = startStr;
     }
   
     function displayTimer() {
@@ -17,7 +32,7 @@ function timer() {
     }
   
     function updateTimer() {
-      if (sec >= 10) {
+      if (sec >= 59) {
         min ++;
         sec = 0;
       } else {
@@ -27,16 +42,19 @@ function timer() {
     }
   
     function resetTimer() {
+      watchStatus = "reset";
+      changeStartLabel();
+      sec = 0;
+      min = 0;
       clearInterval(stopWatch);
-      sec = t;
       displayTimer();
-      startTimer();
     }
   
     function stopTimer() {
+      watchStatus = "pause";
+      changeStartLabel();
       clearInterval(stopWatch);
-      stopWatch = null;
-  
+      console.log(stopWatch);
     }
   
     return {
@@ -48,4 +66,20 @@ function timer() {
 
   let t = timer();
 
-  t.startTimer();
+//   t.startTimer();
+
+  // document.getElementById("start-btn").addEventListener("click", handleStartClick());
+
+  function handleStartClick() {
+    if(watchStatus === "reset") {
+        t.startTimer();
+    }else if (watchStatus === "pause") {
+        t.startTimer();
+    } else {
+      t.stopTimer();
+    }
+  }
+
+  function handleResetClick() {
+    t.resetTimer();
+  }
